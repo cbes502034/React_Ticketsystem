3be761e34f645286fd792c7bc3f031aab7b1e0ed
Ticket-system955 <ticketsystem309@gmail.com>
2025-07-06 16:53:35 +0800

    Create image.js

---
+++ src/assets/image.js
@@ -0,0 +1,11 @@
+// src/assets/images.js
+
+import account from './account.png'
+import cart from './shopping-cart.png'
+import right from './right.png'
+
+export default {
+  account,
+  cart,
+  right
+}
