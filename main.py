74df28a746f966f27271f753b49f8a0a13f21452
Ticket-system955 <ticketsystem309@gmail.com>
2025-07-14 04:48:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -254,4 +254,4 @@ async def get_informations(request:Request):
     
     return JSONResponse(informations)
 #=======================================================================
-app.mount("/",StaticFiles(directory="ticketsystem",html=True))
+app.mount("/",StaticFiles(directory="Frontend",html=True))
