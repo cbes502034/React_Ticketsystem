18865c6307d818a56859aeb721f93ee4c0bfe9b6
cbes502034 <cbes502034@gmail.com>
2025-07-25 22:18:05 +0800

    登出時同時將UserName及UserID從session中刪除

--- Backend/Modules/LogoutModule.py
+++ Backend/Modules/LogoutModule.py
@@ -1,5 +1,6 @@
 async def Logout(request):
-    del request.session["User"]
+    del request.session["UserName"]
+    del request.session["UserID"]
     return {"status":True,
             "notify":"登出成功 !",
             "session":"尚未登入"}
