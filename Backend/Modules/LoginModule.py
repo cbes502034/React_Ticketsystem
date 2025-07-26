cae1b4108177fc7293532dacc9a5075687f61716
Ticket-system955 <ticketsystem309@gmail.com>
2025-07-26 11:53:32 +0800

    Update LoginModule.py

--- Backend/Modules/LoginModule.py
+++ Backend/Modules/LoginModule.py
@@ -1,4 +1,29 @@
-from .ProjectTools import TOTP
+import pyotp
+import qrcode
+import io
+import base64
+
+def GetSecret(request):
+    secret = pyotp.random_base32()
+    request.session["secret"] = secret
+    return secret
+
+def GetTOTPObject(secret):
+    totpobject = pyotp.TOTP(secret)
+    return totpobject
+
+def GetURI(totpobject,email):
+    uri = totpobject.provisioning_uri(name=email, issuer_name="GJun訂票平台")
+    return uri
+
+def getQRcodeSrc(uri):
+    imgio = io.BytesIO()
+    img = qrcode.make(uri)
+    img.save(imgio, format='PNG')
+    imgio.seek(0)
+    base64img = base64.b64encode(imgio.read()).decode('utf-8')
+    qrcodesrc = f"data:image/png;base64,{base64img}"
+    return qrcodesrc
 
 async def Check(tools,request):
     response = await tools.GetRequestData(request=request)
@@ -23,7 +48,7 @@ async def Check(tools,request):
                                               WHERE login_id=%s""",
                                             SELECT=True,
                                             SET=(login_id,))[0][0]
-            totpobject = TOTP.GetTOTPObject(secret)
+            totpobject = GetTOTPObject(secret)
             totpStatus = bool(totpcode == str(totpobject.now()))
             
             notify = []
