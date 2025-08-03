55eaa1cfa2754784e0bf345b9f45473a3988b5a7
Ticket-system955 <rhinegreenland@gmail.com>
2025-08-03 08:14:41 +0800

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,9 +4,11 @@ import Home from './pages/Home'
 import AuthPage from './pages/Auth'
 import ConcertList from './pages/ConcertList'
 import SearchPage from './pages/SearchPage'
+import Concert from './pages/Concert'
+
 //import Login from './pages/Login'
 //import Register from './pages/Register'
-//import Concert from './pages/Concert'
+import Concert from './pages/Concert'
 
 export default function App() {
   return (
@@ -15,8 +17,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<AuthPage />} />
-        <Route path="/concert-list" element={<ConcertList />} /> {/* ✅ 加上這行 */}
+        <Route path="/concert-list" element={<ConcertList />} /> 
         <Route path="/search" element={<SearchPage />} />
+        <Route path="/concert/:id" element={<Concert />} />
 
         {/*<Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
