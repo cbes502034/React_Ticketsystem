734aa155e0af8c2819170fad72242822845ec77f
Ticket-system955 <rhinegreenland@gmail.com>
2025-08-03 08:14:59 +0800

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import concertsData from '../data/concerts'
-
+import Swiper from './Concertswiper'
 
 export default function Home() {
   const [concerts, setConcerts] = useState([])
@@ -10,46 +10,51 @@ export default function Home() {
     setConcerts(concertsData)
   }, [])
 
-
   return (
-    <div className="pt-20 p-6 bg-brand-bg min-h-screen text-brand-text">
-      <h1 className="text-3xl font-bold mb-6 text-brand-text"> 熱門演唱會</h1>
+    <>
+      <div className="pt-20 p-9 bg-brand-bg min-h-screen text-brand-text">
+        <h1 className="text-2xl font-bold  text-brand-text">最近場次</h1>
+        <Swiper />
+      </div>
+      <div className=" p-3 bg-brand-bg min-h-screen text-brand-text">
+        <h1 className="text-3xl font-bold mb-6 text-brand-text">熱門演唱會</h1>
 
-      {concerts.length === 0 ? (
-        <p className="text-brand-text/70">目前沒有熱門演唱會資料</p>
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          {concerts.map(concert => (
-            <div key={concert.id} className="bg-white rounded-lg shadow-md overflow-hidden card">
-              <img
-                src={concert.image_url}
-                alt={concert.name}
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-4">
-                <h2 className="text-xl font-semibold text-brand-bg">{concert.name}</h2>
-                <p className="text-brand-bg/80">{concert.date}</p>
-                <p className="text-brand-bg/70">{concert.location}</p>
-                <Link
-                  to={`/concert/${concert.id}`}
-                  className="inline-block mt-4 px-4 py-2 rounded text-white font-semibold"
-                  style={{
-                    backgroundColor: '#B19693',
-                  }}
-                  onMouseOver={(e) => {
-                    e.currentTarget.style.backgroundColor = '#947A6D'
-                  }}
-                  onMouseOut={(e) => {
-                    e.currentTarget.style.backgroundColor = '#B19693'
-                  }}
-                >
-                  查看詳情
-                </Link>
+        {concerts.length === 0 ? (
+          <p className="text-brand-text/70">目前沒有熱門演唱會資料</p>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {concerts.map(concert => (
+              <div key={concert.id} className="bg-white rounded-lg shadow-md overflow-hidden card">
+                <img
+                  src={concert.image_url}
+                  alt={concert.name}
+                  className="w-full h-60 object-contain"
+                />
+                <div className="p-4">
+                  <h2 className="text-xl font-semibold text-brand-bg">{concert.name}</h2>
+                  <p className="text-brand-bg/80">{concert.date}</p>
+                  <p className="text-brand-bg/70">{concert.location}</p>
+                  <Link
+                    to={`/concert/${concert.id}`}
+                    className="inline-block mt-4 px-4 py-2 rounded text-white font-semibold"
+                    style={{
+                      backgroundColor: '#B19693',
+                    }}
+                    onMouseOver={(e) => {
+                      e.currentTarget.style.backgroundColor = '#947A6D'
+                    }}
+                    onMouseOut={(e) => {
+                      e.currentTarget.style.backgroundColor = '#B19693'
+                    }}
+                  >
+                    查看詳情
+                  </Link>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   )
 }
