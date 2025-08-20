bbc52c97c9c57e907b2f0d66d6cfc793b560fff0
cbes502034 <cbes502034@gmail.com>
2025-08-20 08:03:28 +0800

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 app = FastAPI()
 url = {"mysql":os.getenv("MYSQLPUBLICURL"),
-       "redis":os.getnv("REDISPUBLICURL")}
+       "redis":os.getenv("REDISPUBLICURL")}
 reqT = RequestTools()
 totpT = TotpTools()
 sqlT = SqlTools(URL=url["mysql"])
